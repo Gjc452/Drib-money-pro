bf4b89c34487f84728f25ad4b805c88b68771218
Gjc452 <1796444035@qq.com>
2021-03-09 00:06:07 +0800

    封装 Layout 组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layoutWrapper">
+    <div class="content" :class="classPrefix&&`${classPrefix}-content`">
+      <slot/>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props:['classPrefix'],
+  name: 'Layout'
+};
+</script>
+
+<style lang="scss" scoped>
+.layoutWrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  .content {
+    flex-grow: 1;
+    overflow: auto;
+  }
+}
+</style>
