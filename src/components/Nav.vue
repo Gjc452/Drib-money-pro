2f25ce40b79d35c3a8c7a3254660239cdf05a443
Gjc452 <1796444035@qq.com>
2021-03-09 00:50:38 +0800

    完成部分导航栏样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,7 +9,12 @@
       图表
     </router-link>
     <router-link to="/money" class="item" active-class="selected">
-      <Icon name="add"/>
+      <div class="addWrapper">
+        <div class="iconWrapper">
+          <Icon name="add"/>
+        </div>
+      </div>
+      <Icon/>
       记账
     </router-link>
     <router-link to="/news" class="item" active-class="selected">
@@ -33,27 +38,48 @@ export default {
 @import "~@/assets/style/helper.scss";
 
 nav {
+  position: relative;
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
   flex-direction: row;
   font-size: 12px;
 
   > .item {
-    padding: 2px 0;
-    width: 33.333333%;
+    padding: 4px 0;
+    width: 25%;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
 
-    .icon {
-      width: 32px;
-      height: 32px;
+    > .icon {
+      width: 28px;
+      height: 28px;
     }
   }
 
   > .item.selected {
     color: $color-highlight;
+  }
+}
+.addWrapper{
+  position: absolute;
+  top:-22px;
+  padding: 4px;
+  background: white;
+  border-left: 1px solid rgb(227,227,227);
+  border-top: 1px solid rgb(227,227,227);
+  border-radius: 50% 50% ;
+  transform: rotate(45deg);
+  .iconWrapper{
+    transform: rotate(-45deg);
+    background: $color-highlight;
+    padding: 10px;
+    border-radius: 50%;
+    .icon{
+      width: 20px;
+      height: 20px;
+    }
   }
 }
 </style>
