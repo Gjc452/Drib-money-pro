c2784de385114e531a03165b13b1dea819eade00
Gjc452 <1796444035@qq.com>
2021-03-15 23:49:09 +0800

    Statistics.vue 拆分

---
+++ src/components/Statistics/StatisticsHeader.vue
@@ -0,0 +1,139 @@
+<template>
+  <header>
+    <div class="top">
+      <div class="time">
+        <span>2021年</span>
+        <div>
+          <span>03</span><span>月</span>
+          <Icon name="icon-juanzeng"/>
+          <span>|</span>
+        </div>
+      </div>
+      <div class="income">
+        <span>收入</span>
+        <span>20.00</span>
+      </div>
+      <div class="outlay">
+        <span>支出</span>
+        <span>6724.30</span>
+      </div>
+    </div>
+    <ul>
+      <li>
+        <Icon name="icon-juanzeng"/>
+        <span>账单</span>
+      </li>
+      <li>
+        <Icon name="icon-juanzeng"/>
+        <span>预算</span>
+      </li>
+      <li>
+        <Icon name="icon-juanzeng"/>
+        <span>资产管家</span>
+      </li>
+      <li>
+        <Icon name="icon-juanzeng"/>
+        <span>理财知识</span>
+      </li>
+      <li>
+        <Icon name="icon-juanzeng"/>
+        <span>购物返现</span>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class StatisticsHeader extends Vue{
+}
+</script>
+
+<style lang="scss" scoped>
+$gray: rgb(150, 150, 150);
+header {
+  padding: 0 6px;
+  background: linear-gradient(180deg, rgba(255,222,92,1) 70%, rgba(253,234,157,1) 83%, rgba(255,255,255,1) 100%);
+
+  .top {
+    padding: 14px 8px;
+    display: flex;
+    justify-content: flex-start;
+
+    > div {
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+
+      > span:first-child {
+        color: $gray;
+        font-size: 12px;
+        padding-bottom: 4px;
+      }
+    }
+
+    .time {
+      div {
+        span:first-child {
+          font-size: 22px;
+          padding-right: 2px;
+        }
+
+        span:nth-child(2) {
+          font-size: 14px;
+          padding-right: 2px;
+        }
+
+        .icon {
+          width: 14px;
+          height: 14px;
+        }
+
+        span:last-child {
+          font-size: 22px;
+          font-weight: lighter;
+          color: $gray;
+          padding: 0 8px;
+        }
+      }
+    }
+
+    .income {
+      padding: 0 10px;
+    }
+
+    .outlay {
+      margin-left: auto;
+      padding-right: 60px;
+    }
+  }
+
+  ul {
+    display: flex;
+    background: white;
+    border-radius: 4px;
+    box-shadow: 0 0 2px 0 rgba(0,0,0,0.1);
+
+    > li {
+      width: 20%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 8px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
+
+      span {
+        padding-top: 4px;
+        font-size: 10px;
+        color: $gray;
+      }
+    }
+  }
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,47 +1,6 @@
 <template>
   <Layout>
-    <header>
-      <div class="top">
-        <div class="time">
-          <span>2021年</span>
-          <div>
-            <span>03</span><span>月</span>
-            <Icon name="icon-juanzeng"/>
-            <span>|</span>
-          </div>
-        </div>
-        <div class="income">
-          <span>收入</span>
-          <span>20.00</span>
-        </div>
-        <div class="outlay">
-          <span>支出</span>
-          <span>6724.30</span>
-        </div>
-      </div>
-      <ul>
-        <li>
-          <Icon name="icon-juanzeng"/>
-          <span>账单</span>
-        </li>
-        <li>
-          <Icon name="icon-juanzeng"/>
-          <span>预算</span>
-        </li>
-        <li>
-          <Icon name="icon-juanzeng"/>
-          <span>资产管家</span>
-        </li>
-        <li>
-          <Icon name="icon-juanzeng"/>
-          <span>理财知识</span>
-        </li>
-        <li>
-          <Icon name="icon-juanzeng"/>
-          <span>购物返现</span>
-        </li>
-      </ul>
-    </header>
+    <StatisticsHeader/>
     <main>
       <ol>
         <li>
@@ -136,141 +95,66 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import StatisticsHeader from '@/components/Statistics/StatisticsHeader.vue';
 
-@Component
+@Component({
+  components: {StatisticsHeader}
+})
 export default class Statistics extends Vue {
 }
 </script>
 
 <style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
-
 $gray: rgb(150, 150, 150);
-header {
-  padding: 0 6px;
-  background: linear-gradient(180deg, rgba(255,222,92,1) 70%, rgba(253,234,157,1) 83%, rgba(255,255,255,1) 100%);
-
-  .top {
-    padding: 14px 8px;
-    display: flex;
-    justify-content: flex-start;
-
-    > div {
-      display: flex;
-      justify-content: space-between;
-      flex-direction: column;
-
-      > span:first-child {
-        color: $gray;
-        font-size: 12px;
-        padding-bottom: 4px;
-      }
-    }
-
-    .time {
-      div {
-        span:first-child {
-          font-size: 22px;
-          padding-right: 2px;
-        }
-
-        span:nth-child(2) {
-          font-size: 14px;
-          padding-right: 2px;
-        }
-
-        .icon {
-          width: 14px;
-          height: 14px;
-        }
-
-        span:last-child {
-          font-size: 22px;
-          font-weight: lighter;
-          color: $gray;
-          padding: 0 8px;
-        }
-      }
-    }
-
-    .income {
-      padding: 0 10px;
-    }
-
-    .outlay {
-      margin-left: auto;
-      padding-right: 60px;
-    }
-  }
-
-  ul {
-    display: flex;
-    background: white;
-    border-radius: 4px;
-    box-shadow: 0 0 2px 0 rgba(0,0,0,0.1);
-
+main {
+  ol {
     > li {
-      width: 20%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 8px 0;
-      .icon{
-        width: 24px;
-        height: 24px;
-      }
-
-      span {
-        padding-top: 4px;
-        font-size: 10px;
-        color: $gray;
-      }
-    }
-  }
-}
-main{
-  ol{
-    >li{
-      .title{
+      .title {
         display: flex;
         color: $gray;
         font-size: 11px;
         justify-content: space-between;
         padding: 6px 12px;
         border-bottom: 1px solid;
-        .payTime{
-          span:first-child{
+
+        .payTime {
+          span:first-child {
             padding-right: 10px;
           }
         }
-        .total{
-          span:first-child{
+
+        .total {
+          span:first-child {
             padding-right: 20px;
           }
         }
       }
-      .recordWrapper{
-        .record{
+
+      .recordWrapper {
+        .record {
           padding: 8px 12px;
           display: flex;
           align-items: center;
           justify-content: flex-start;
-          div{
+
+          div {
             display: flex;
             padding: 7px;
-            background: rgb(245,245,245);
+            background: rgb(245, 245, 245);
             border-radius: 50%;
             margin-right: 22px;
-            .icon{
+
+            .icon {
               width: 20px;
               height: 20px;
             }
           }
-          span{
+
+          span {
             font-size: 14px;
           }
-          span:last-child{
+
+          span:last-child {
             margin-left: auto;
           }
         }
