dbcc90a3b988a2a208db9106b16d6961064e0e2e
Gjc452 <1796444035@qq.com>
2021-03-17 00:27:15 +0800

    优化CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -20,7 +20,7 @@
                 <Icon :name="item.tag.icon"/>
               </div>
               <span>{{ item.notes }}</span>
-              <span>{{ item.amount }}</span>
+              <span>{{ item.type === '-' ? '-'.concat(item.amount) : item.amount }}</span>
             </li>
           </ul>
         </li>
@@ -97,8 +97,8 @@ main {
         color: $gray;
         font-size: 11px;
         justify-content: space-between;
-        padding: 6px 12px;
-        border-bottom: 1px solid;
+        padding: 8px 12px;
+        border-bottom: 0.5px solid;
 
         .payTime {
           span:first-child {
@@ -115,17 +115,17 @@ main {
 
       .recordWrapper {
         .record {
-          padding: 8px 12px;
+          padding: 10px 12px;
           display: flex;
           align-items: center;
           justify-content: flex-start;
 
           div {
             display: flex;
-            padding: 7px;
+            padding: 8px;
             background: rgb(245, 245, 245);
             border-radius: 50%;
-            margin-right: 22px;
+            margin-right: 20px;
 
             .icon {
               width: 20px;
