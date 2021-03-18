bc8b352c8befb8f68caa5160f27d4e562b70cca7
Gjc452 <1796444035@qq.com>
2021-03-19 00:31:06 +0800

    尝试使用 echarts

---
+++ src/components/Echarts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div ref="container"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import * as echarts from 'echarts';
+
+@Component
+export default class Echarts extends Vue {
+  @Prop() readonly option!;
+
+  mounted() {
+    const width = document.documentElement.clientWidth;
+    this.$refs.container.style.width = `${width}px`;
+    this.$refs.container.style.height = `${width * 0.4}px`;
+    this.chart = echarts.init(this.$refs.container);
+    this.chart.setOption(this.option);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <div>图表</div>
+    <Echarts :option="option"/>
   </Layout>
 </template>
 
@@ -8,10 +8,79 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import store from '@/store';
-@Component
-export default class Chart extends Vue{
-  mounted(){
-    store.commit('resetRecord')
+import Echarts from '@/components/Echarts.vue';
+
+@Component({
+  components: {Echarts}
+})
+export default class Chart extends Vue {
+  option = {
+    grid: {
+      x: 10,
+      y: 20,
+      x2: 10,
+      y2: 20
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    xAxis: {
+      type: 'category',
+      data: ['03-08', '03-09', '03-10', '03-11', '03-12', '0-13', '03-14'],
+      axisTick: {
+        show: false,
+      },
+    },
+    yAxis: {
+      type: 'value',
+      max: 'dataMax',
+      splitNumber: 1,
+      axisLabel: {
+        show: false
+      },
+      splitLine: {
+        lineStyle: {
+          color: 'rgb(136,136,136)'
+        }
+      },
+    },
+    series: [{
+      data: [0, 230, 224, 218, 135, 147, 260],
+      type: 'line',
+      name: '金额',
+      symbolSize: 5,
+      itemStyle: {
+        normal: {
+          lineStyle: {
+            width: 0.5,
+            color: '#000'
+          }
+        }
+      },
+      markLine: {
+        silent: true,
+        symbol: 'none',
+        lineStyle: {
+          width: 1,
+          color: 'rgb(217,217,217)',
+        },
+        itemStyle: {
+          normal: {
+            color: '#000',
+            label: {
+              show: false
+            }
+          }
+        },
+        data: [
+          {type: 'average', name: '平均值'},
+        ]
+      }
+    }]
+  };
+
+  mounted() {
+    store.commit('resetRecord');
     store.commit('setType', '-');
   }
 }
