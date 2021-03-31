eae67680b7aa903cc286fff8fd64e5332702f5a4
Gjc452 <1796444035@qq.com>
2021-04-01 00:28:46 +0800

    完成数据渲染

--- src/views/Bill.vue
+++ src/views/Bill.vue
@@ -2,74 +2,119 @@
   <div class="wrapper">
     <header>
       <div class="top">
-        <div class="year">
-          <span>2021年</span>
+        <div class="year" @click="open">
+          <span>{{ datetime.slice(0, 4) }}年</span>
           <Icon name="icon-xiala"/>
         </div>
         <span>账单</span>
-        <div class="back">返回</div>
+        <div @click="back" class="back">返回</div>
       </div>
       <div class="surplus">
         <span>结余</span>
-        <div>-6821.00</div>
+        <div>{{ recordList.map(r => r.surplus).reduce((a, b) => a + b, 0).toFixed(2) }}</div>
       </div>
       <div class="sortMoney">
         <div>
           <span>收入</span>
-          <span>1019.00</span>
+          <span>{{ recordList.map(r => r.income).reduce((a, b) => a + b, 0).toFixed(2) }}</span>
         </div>
         <div>
           <span>支出</span>
-          <span>7840.00</span>
+          <span>{{ recordList.map(r => r.outlay).reduce((a, b) => a + b, 0).toFixed(2) }}</span>
         </div>
       </div>
     </header>
     <main>
-      <ol>
+      <ol v-if="recordList.length!==0">
         <li>
           <span>月份</span>
           <span>收入</span>
           <span>支出</span>
           <span>结余</span>
         </li>
-        <li>
-          <span>3月</span>
-          <span>1019.00</span>
-          <span>7831.00</span>
-          <span>-6812.00</span>
-        </li>
-        <li>
-          <span>2月</span>
-          <span>0.00</span>
-          <span>0.00</span>
-          <span>0.00</span>
-        </li>
-        <li>
-          <span>1月</span>
-          <span>0.00</span>
-          <span>0.00</span>
-          <span>-9.00</span>
+        <li v-for="r in recordList" :key="r.month">
+          <span>{{ r.month }}</span>
+          <span>{{ r.income }}</span>
+          <span>{{ r.outlay }}</span>
+          <span>{{ r.surplus }}</span>
         </li>
       </ol>
+      <NoList v-else/>
     </main>
+    <yd-datetime v-show="false" type="month" ref="datetime" v-model="datetime" slot="right"></yd-datetime>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import dayjs from 'dayjs';
+import NoList from '@/components/common/NoList.vue';
 
-@Component
+@Component({
+  components: {NoList}
+})
 export default class Bill extends Vue {
+  datetime = dayjs().format('YYYY-MM');
+
+  open() {
+    this.$refs.datetime.open();
+  }
+
+  back() {
+    this.$router.back();
+  }
+
+  created() {
+    this.$store.commit('fetchRecordList');
+  }
+
+  get recordList() {
+    const year = this.datetime.slice(0, 4);
+    const recordList = (this.$store.state.recordList as RecordItem[])
+        .filter(r => dayjs(r.createAt).format('YYYY') === year).sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
+    if (recordList.length === 0) {return [];}
+    const newList = [{
+      month: dayjs(recordList[0].createAt).format('M月'),
+      income: recordList[0].type === '+' ? parseFloat(recordList[0].amount) : 0,
+      outlay: recordList[0].type === '-' ? parseFloat(recordList[0].amount) : 0,
+      surplus: 0
+    }];
+    for (let i = parseInt(newList[0].month.slice(0, 2)); i > 0; i--) {
+      const month = dayjs(`${year}-${i}`).format('M');
+      const index = newList.map(n => n.month.replace('月', '')).indexOf(month);
+      if (index < 0) {
+        newList.push({
+          month: month + '月',
+          income: 0,
+          outlay: 0,
+          surplus: 0,
+        });
+      }
+      const n = newList.map(n => n.month).indexOf(month + '月');
+      const filterList = recordList.slice(1).filter(l => dayjs(l.createAt).format('M') === month);
+      for (let j = 0; j < filterList.length; j++) {
+        if (filterList[j].type === '-') {
+          newList[n].outlay += parseFloat(filterList[j].amount);
+        } else {
+          newList[n].income += parseFloat(filterList[j].amount);
+        }
+      }
+    }
+    newList.map(n => n.surplus = n.income - n.outlay);
+    return newList;
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-.wrapper{
+
+.wrapper {
   height: 100vh;
-  background: rgb(250,250,250);
+  background: rgb(250, 250, 250);
 }
+
 header {
   display: flex;
   flex-direction: column;
@@ -143,32 +188,40 @@ header {
     }
   }
 }
-main{
+
+main {
   font-weight: 300;
-  ol{
+
+  ol {
     background: white;
     display: flex;
     flex-direction: column;
     justify-content: center;
     width: 100%;
     font-size: 14px;
-    li:nth-child(1){
+
+    li:nth-child(1) {
       padding: 2px 12px;
       font-size: 10px;
     }
-    li:nth-child(n+2){
+
+    li:nth-child(n+2) {
       border-top: 1px solid $gray;
     }
-    li{
+
+    li {
       display: flex;
       padding: 12px 12px;
-      span:nth-child(1){
+
+      span:nth-child(1) {
         padding-right: 10px;
+        width: 13%;
       }
-      span:nth-child(n+2){
+
+      span:nth-child(n+2) {
         display: flex;
         justify-content: center;
-        width: 30%;
+        width: 29%;
       }
     }
   }
